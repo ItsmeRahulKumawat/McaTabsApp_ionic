eee2eb10cf717e5187dce1524b3c3dbe4cac8bc1
[Rahul Kumawat] <[rahulkumawat20000@gmail.com]>
2021-12-16 17:59:57 +0530

    McaTabsApp_ionic

---
+++ src/app/feedback/feedback.page.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-feedback',
+  templateUrl: './feedback.page.html',
+  styleUrls: ['./feedback.page.scss'],
+})
+export class FeedbackPage implements OnInit {
+
+  constructor(public toastController: ToastController) { }
+
+  ngOnInit() {
+  }
+
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'Your feedback is sent.',
+      duration: 2000
+    });
+    toast.present();
+}
+
+}
